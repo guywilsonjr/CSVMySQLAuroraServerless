4b1f54900cb63e506f4fe9208eb58911fca8b997
Guy Wilson <guywils@uw.edu>
2020-11-15 17:52:50 -0800

    Quite a bit going rn. All done first trial

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import csv
 import logging
+import yaml
 import asyncio
 import aioboto3
-import aiofiles
-from aiocsv import AsyncReader, AsyncDictReader
+from aiofile import LineReader, AIOFile
+import click
 from tenacity import retry, retry_if_exception, stop_after_attempt, before_log, wait_exponential, after_log
 
 logger = logging.getLogger(__name__)
@@ -14,6 +15,12 @@ MAX_QUERY_SIZE = 65536
 INSERT_QUERY_SIZE = MAX_QUERY_SIZE // 2
 resource_arn = 'arn:aws:rds:us-west-2:936272581790:cluster:data'
 secret_arn = 'arn:aws:secretsmanager:us-west-2:936272581790:secret:datacreds-eGCBb0'
+create_table_query_format ='''
+CREATE TABLE IF NOT EXISTS `{}`.`{}` (
+  {}
+  PRIMARY KEY ({}));
+'''
+
 
 def exception_is_retryable(error: Exception) -> bool:
     logger.debug(error)
@@ -41,116 +48,101 @@ async def run_query(query: str, client=None, trans_id: str=None):
     else:
         return await client.execute_statement(sql=query, resourceArn=resource_arn, secretArn=secret_arn)
 
-columns = []
-file_path = '/home/guy/Downloads/nfl-big-data-bowl-2021/plays.csv'
-ifields = [3,4,5, 9, 12,13,16,17,19,23,24]
-dfields = [25]
-bfields =[26]
-column_types = [
-    'VARCHAR(20)',
-    'VARCHAR(20)',
-    'VARCHAR(1023)',
-    'INT',
-    'INT',
-    'INT',
-    'VARCHAR(10)',
-    'VARCHAR(63)',
-    'VARCHAR(63)',
-    'INT',#YARDLINENUMBER
-    'VARCHAR(63)',
-    'VARCHAR(63)',#
-    'INT',
-    'INT',
-    'VARCHAR(63)',#
-    'VARCHAR(63)',#DROP
-    'INT',
-    'INT',
-    'VARCHAR(63)',#CLOCK
-    'INT',
-    'VARCHAR(63)',#PCODE
-    'VARCHAR(63)',
-    'VARCHAR(10)',
-    'INT',
-    'INT',#PRES
-    'DOUBLE',
-    'BOOL'
-]
-schema = 'bdb'
-table_name = 'plays'
-create_table_query_format =\
-'''
-CREATE TABLE IF NOT EXISTS `{}`.`{}` (
-  {}
-  PRIMARY KEY ({}));
-'''
-insert_format ='''
-REPLACE INTO 
-`{}`.`{}`
-VALUES
-{{}}
-'''.format(schema, table_name)
 
-async def send_if_sized(client, tuple_list, always_send=False):
+def load_config(filepath: str):
+    with open(filepath, 'r') as file:
+        conf = yaml.load(file, Loader=yaml.FullLoader)
+        return conf
+
+
+async def send_if_sized(client, tuple_list, schema, table_name, always_send=False):
     str_tups = [str(tup) for tup in tuple_list]
     val_list_str = ','.join(str_tups)
+    insert_format = '''
+    REPLACE INTO 
+    `{}`.`{}`
+    VALUES
+    {{}}
+    '''.format(schema, table_name)
     query = insert_format.format(val_list_str)
-
-    if len(query) > INSERT_QUERY_SIZE:
+    query = query.replace("'NULL'", 'NULL')
+    if len(query) > INSERT_QUERY_SIZE or always_send:
         await run_query(query, client)
         return True
     return False
 
 
-async def process_line(line, column_names, procs, sends, client, tuple_list):
+async def process_line(line, column_names, col_types, procs, sends, client, tuple_list, schema, table_name):
     for row in csv.DictReader([line], delimiter=',', fieldnames=column_names):
         val_list = list(row.values())
+        ifields = [i for i in range(len(col_types)) if col_types[i] == 'INT']
+        dfields = [i for i in range(len(col_types)) if col_types[i] == 'DOUBLE']
+        bfields = [i for i in range(len(col_types)) if col_types[i] == 'BOOL']
         for ifield in ifields:
-            val_list[ifield] = int(val_list[ifield]) if val_list[ifield] else -1
+            val_list[ifield] = int(val_list[ifield]) if val_list[ifield] else 'NULL'
         for dfield in dfields:
-            val_list[dfield] = float(val_list[ifield])
+            val_list[dfield] = float(val_list[dfield]) if val_list[dfield] else 'NULL'
         for bfield in bfields:
-            val_list[bfield] = bool(val_list[ifield])
+            val_list[bfield] = bool(val_list[bfield]) if val_list[bfield] else 'NULL'
         vals = tuple(val_list)
         tuple_list.append(vals)
         procs += 1
-        print(procs, sends)
-        if await send_if_sized(client, tuple_list):
+
+        if await send_if_sized(client, tuple_list, schema, table_name):
+            logger.info('Rows processed:%s \nBatchesUploaded: %s', procs, sends)
             sends += 1
             # Race condition shouldn't exceed half of limit and since it's async only 1 running at a single time
             tuple_list = []
     return procs, sends, tuple_list
 
-async def scan_file_for_col_sizes():
-    print('Scanning')
-    async with aiofiles.open(file_path, mode="r", encoding="utf-8", newline="") as afp:
-        for line in await afp.readlines():
-            async for row in AsyncDictReader([line], delimiter=','):
-                print(row)
+
+def get_create_table_query(column_names, column_types, schema, table_name, conf):
+    create_table_col_strs = ''
+    for i in range(len(column_names)):
+        create_table_col_strs += column_names[i] + ' ' + column_types[i] + ', '
+    primary_key = conf['PRIMARY_KEY']
+    return create_table_query_format.format(schema, table_name, create_table_col_strs, primary_key)
 
 
-async def run():
-    async with aiofiles.open(file_path, mode='r') as f:
-        columns_str = await f.readline()
-        column_names = columns_str.strip().split(',')
-        create_table_col_strs = ''
-        for i in range(len(column_names)):
-            create_table_col_strs += column_names[i] + ' ' + column_types[i] +', '
+async def process_file(file_path: str, conf: dict, client):
+    schema = conf['SCHEMA_NAME']
+    table_name = conf['TABLE_NAME']
+    col_types = conf['COL_TYPES']
 
-        primary_key = ','.join([column_names[0], column_names[1]])
-
-        create_table_query = create_table_query_format.format(schema, table_name, create_table_col_strs, primary_key)
-
+    async with AIOFile(file_path, mode='r') as f:
         tuple_list = []
         procs = 0
         sends = 0
-        async with aioboto3.client('rds-data', region_name='us-west-2') as client:
-            await run_query(create_table_query, client)
-            async for line in f:
-                procs, sends, tuple_list = process_line(line, column_names, procs, sends, client, tuple_list)
+        first_line = True
+        async for line in LineReader(f):
+            column_names: list
+            if first_line:
+                first_line = False
+                column_names = line.strip().split(',')
+                create_table_query = get_create_table_query(column_names, col_types, schema, table_name, conf)
+                await run_query(create_table_query, client)
+            else:
+                procs, sends, tuple_list = await process_line(line, column_names, col_types, procs, sends, client, tuple_list, schema, table_name)
+        if len(tuple_list):
+            await send_if_sized(client, tuple_list, schema, table_name, True)
+            procs += len(tuple_list)
+            sends += 1
+            logger.info('For File: %s Rows processed:%s \nBatchesUploaded: %s', file_path, procs, sends)
 
-            if len(tuple_list):
-                await send_if_sized(client, tuple_list, True)
+
+async def run(conf: dict):
+    file_paths = conf['FILE_PATHS']
+    async with aioboto3.client('rds-data', region_name='us-west-2') as client:
+        await asyncio.gather(*[process_file(file_path, conf, client) for file_path in file_paths])
 
 
+@click.command()
+@click.option("--conf-path", type=str, help="File location")
+def main(conf_path: str):
+    logger.info('Using conf-path: %s', conf_path)
+    conf_data = load_config(conf_path)
+    logger.info('Conf data: %s', conf_data)
+    asyncio.run(run(conf_data))
 
-asyncio.run(scan_file_for_col_sizes())
+if __name__ == '__main__':
+    main()
